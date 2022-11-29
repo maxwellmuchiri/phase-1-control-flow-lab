d8fef354eb51065c32c709e4d3f73fb3c7b5cc0b
maxwellmuchiri <muchirimaxwel@gmail.com>
2022-11-29 15:33:57 +0300

    finished assignment

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+
+  
+  
+  if (feet <=400){
+      print = 'This one is on me!';
+    }else if (feet<=2500){
+      print = 'I will gladly take your thirty bucks.'
+    }else {
+      print = 'No can do.'
+    }
+  console.log(feet)
+  return print
+  }
+  
+  
+function ternaryCheckCity(city){
+
+ return city==='NYC'?'Ok, sounds good.':`No go.`
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+    return'Thank you so much.'
+      
+      case 'not as generous':
+      return "Thank you."
+    default:
+      return "Bye."
+      
+  }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
 }
